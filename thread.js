1609e6fb0976d1500c48aef561e97785251ababd
Hrishikesh <dhormarehrishi10@gmail.com>
2023-06-09 10:27:03 +0530

    make publishn  fro git

--- thread.js
+++ thread.js
@@ -4,3 +4,14 @@ console.log("Hrishi ");
 setTimeout(() => console.log("Good Morning"),90);
 
 console.log("9 June");
+
+async function sum(n1, n2) {
+    return n1 + n2;
+}
+console.log(sum(10, 10));
+
+async function main(){
+    let result =  await sum(1, 1);
+    console.log(result);
+}
+main();
