a18b74c4570fd58be8f289d5a345c72a68b493ba
Hrishikesh <dhormarehrishi10@gmail.com>
2023-06-10 10:10:22 +0530

    connect mongo db using node .js

--- src/mongodb.js
+++ src/mongodb.js
@@ -1,13 +1,13 @@
 import { MongoClient } from "mongodb";
 
 async function main() {
-    const uri = "mongodb://localhost:27017";
+    const uri = "mongodb://127.0.0.1:27017";
     const client = new MongoClient(uri);
 
     const db = client.db("mydb");
     const message = db.collection("message");
     
-    await message.insertOne({});
+    await message.insertOne({message: "Added from mongodb.js program"});
     await client.close();
     
     console.log("Record Added Successfully");
